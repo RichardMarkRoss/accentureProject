7a8f875b7cac9870e4d183af6b16d6ec054ef8dd
RichardMarkRoss <cptrichkidprice@gmail.com>
2022-06-16 00:21:24 +0200

    nw commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { StartComponent } from './start/start.component';
+import { SecondComponent } from './second/second.component';
+const routes: Routes = [
+  {path: '', component: StartComponent},
+  {path: 'second', component: SecondComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { };
+export const routingComponent =  [StartComponent, SecondComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
