a19bb731a2198cb666f7436994693e57d8c7b4ad
RichardMarkRoss <cptrichkidprice@gmail.com>
2022-06-20 09:01:24 +0200

    install necessary dependancies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-
+import { OwlModule } from 'angular-owl-carousel';
 const routes: Routes = [
-  {path: 'first', component: FirstComponent},
-  {path: 'second', component: SecondComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { };
-export const routingComponent =  [FirstComponent,SecondComponent];
+export const routingComponent =  [];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent,
-    routingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
