aba782ed2ad331a83d25af71351d96c1cfa01693
RichardMarkRoss <cptrichkidprice@gmail.com>
2022-06-21 15:43:07 +0200

    deploy to heroku test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,12 +31,4 @@ customOptions: OwlOptions = {
   }
   }
 }
-
- slides = [
-    {id: 1, img: "https://dummyimage.com/350x150/423b42/fff"},
-    {id: 2, img: "https://dummyimage.com/350x150/2a2b7a/fff"},
-    {id: 3, img: "https://dummyimage.com/350x150/1a2b7a/fff"},
-    {id: 4, img: "https://dummyimage.com/350x150/7a2b7a/fff"},
-    {id: 5, img: "https://dummyimage.com/350x150/4a2b7a/fff"}
-  ];
 }
